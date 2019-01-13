385161cfdc14e78b69d88b7bc577ae687fd979a2
Milos Dimic <milos.dimic@htecgroup.com>
2019-01-13 10:49:02 +0100

    initial commit;

--- ProtocolComposition/ViewController.swift
+++ ProtocolComposition/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+class ViewController: UIViewController {}
 
-class ViewController: UIViewController {
+class ImageProvider {}
+class ArticleProvider {}
+class PersistanceProvider {}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+protocol ImageProvidable {
+    var imageProvider: ImageProvider { get }
+}
+
+protocol ArticleProvidable {
+    var articleProvider: ArticleProvider { get }
+}
+
+protocol PersistanceProvidable {
+    var persistanceProvider: PersistanceProvider { get }
+}
+
+
+class BasicModel {
+    
+    typealias Dependencies = ImageProvidable
+    
+    private let dependencies: Dependencies
+    
+    init(dependencies: Dependencies) {
+        self.dependencies = dependencies
     }
+}
+
+class AdvancedModel {
+    
+    typealias Dependencies = ImageProvidable & ArticleProvidable & PersistanceProvidable
+    
+    private let dependencies: Dependencies
+    
+    init(dependencies: Dependencies) {
+        self.dependencies = dependencies
+    }
+}
 
 
+struct AppDependency: ImageProvidable, ArticleProvidable, PersistanceProvidable {
+    
+    var imageProvider: ImageProvider
+    var articleProvider: ArticleProvider
+    var persistanceProvider: PersistanceProvider
+}
+
+class AppCoordinator {
+    
+    let appDependencies = AppDependency(imageProvider: ImageProvider(),
+                                        articleProvider: ArticleProvider(),
+                                        persistanceProvider: PersistanceProvider())
+    
+    lazy var basicModel = BasicModel(dependencies: appDependencies)
+    lazy var advancedModel = AdvancedModel(dependencies: appDependencies)
 }
 
